2f503eecc2fee9216760d945cafc4cba598fc828
Pradeep Parihar <pradeepfairygarland@gmail.com>
2026-04-12 19:08:16 +0530

    Initial backend setup

---
+++ controllers/authController.js
@@ -0,0 +1,29 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json(user);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
